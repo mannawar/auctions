a77c5e01b3ee1cb5c34a0dc03d77ae9b7c9d9766
dearmannawar <dearmannawar@gmail.com>
2023-11-17 16:03:02 +0300

    End of section 3

---
+++ src/SearchService/Data/DBInitializer.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+using MongoDB.Entities;
+using SearchService.Services;
+
+namespace SearchService.Data;
+
+public class DBInitializer
+{
+    public static async Task InitDb(WebApplication app)
+    {
+        await DB.Index<Item>()
+            .Key(x => x.Make, KeyType.Text)
+            .Key(x => x.Model, KeyType.Text)
+            .Key(x => x.Color, KeyType.Text)
+            .CreateAsync();
+
+        var count = await DB.CountAsync<Item>();
+        using var scope = app.Services.CreateScope();
+        var httpClient = scope.ServiceProvider.GetRequiredService<AuctionSvcHttpClient>();
+        var items = await httpClient.GetItemsForSearchDb();
+        Console.WriteLine(items.Count+ " returned from auction db");
+
+        if (items.Count > 0) await DB.SaveAsync(items);
+    }
+}
---
+++ src/SearchService/Program.cs
@@ -0,0 +1,45 @@
+using System.Net;
+using MongoDB.Driver;
+using MongoDB.Entities;using Polly;
+using Polly.Extensions.Http;
+using SearchService;
+using SearchService.Data;
+using SearchService.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+await DB.InitAsync("SearchDb", MongoClientSettings.FromConnectionString(builder.Configuration.GetConnectionString("MongoDbConnection")));
+
+app.Lifetime.ApplicationStarted.Register(async () =>
+{
+    try
+    {
+        await DBInitializer.InitDb(app);
+
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+    }
+});
+
+
+app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+    => HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
